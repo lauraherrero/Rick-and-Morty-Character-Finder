0b409186f5998546d18baf778ef6f3d4a091604f
lauraherrero <lauraherrero36@gmail.com>
2020-01-14 19:05:11 +0100

    Create docs

---
+++ docs/precache-manifest.7488add4e81899e3b0315071376824c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30361d0e1c7ba10567e9068b011c2ec7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc85e0c0c2d4d8b1494d",
+    "url": "./static/css/main.1d520d93.chunk.css"
+  },
+  {
+    "revision": "2799eb60ae7a4fc5aeac",
+    "url": "./static/js/2.b621e907.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.b621e907.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bc85e0c0c2d4d8b1494d",
+    "url": "./static/js/main.154c88a8.chunk.js"
+  },
+  {
+    "revision": "882537cc71f6c5f21d30",
+    "url": "./static/js/runtime-main.746f9ab3.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/Rick_and_Morty.75509f2e.png"
+  }
+]);
